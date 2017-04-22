8a25fc4a433fb957bb3fa007751c92909d7b0bc1
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-04-23 00:09:31 +0300

    update 1

--- app/src/main/java/com/example/alexandru/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/alexandru/newsapp/NewsActivity.java
@@ -1,7 +1,7 @@
 package com.example.alexandru.newsapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class NewsActivity extends AppCompatActivity {
 
@@ -9,5 +9,7 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        String testRequestString = "http://content.guardianapis.com/search?rights=developer-community&page-size=30&show-rights=developer-community&show-fields=all&order-by=newest&q=France%20or%20football&api-key=3ca5b220-376f-4bc4-b5f9-ca8840c0cef9";
     }
 }
